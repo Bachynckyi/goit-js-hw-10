19c0c3647565173ebcf026d22c2af262c59ed2ab
Andrew <bachynckyi.andrii@gmail.com>
2022-07-28 23:00:04 +0300

    added_err_404

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,12 @@
 export default function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v3.1';
     return fetchCountries(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => { return response.json() })
+        .then(response => {
+        if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    },
+  ); 
 };
 
