ab7cef15c82b8c6321c0a7acf732af966102b466
Andrew <bachynckyi.andrii@gmail.com>
2022-07-29 11:08:00 +0300

    bagfix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export default function fetchCountries(name) {
     const BASE_URL = 'https://restcountries.com/v3.1';
-    return fetchCountries(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
         if (!response.ok) {
         throw new Error(response.status);
